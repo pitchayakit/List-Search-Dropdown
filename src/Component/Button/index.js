96f591c9f6fd6b088580af66020cf5d279960e9b
pitchayakit <pitchayakit.p@gmail.com>
2017-09-18 17:05:23 +0700

    improve performance

--- src/Component/Button/index.js
+++ src/Component/Button/index.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 
 class Button extends Component {
+    shouldComponentUpdate(){ return 0 }
 
-  render() {
-    const { backButtonClick, label  } = this.props
+    render() {
+        const { backButtonClick, label  } = this.props
 
-    return <button type="button" onClick={backButtonClick}>{label}</button>
-  }
+        return <button type="button" onClick={backButtonClick}>{label}</button>
+    }
 }
 
 export default Button
