79e29c70aa43749eefc5bcc0498b706512b1ec71
pitchayakit <pitchayakit.p@gmail.com>
2017-09-21 15:34:19 +0700

    reduce code

--- src/Component/Table/Tbody/tr.js
+++ src/Component/Table/Tbody/tr.js
@@ -2,10 +2,11 @@ import React from 'react'
 import Td from './td'
 
 const tr = ({ row }) => {
-    const mapColumns = row.map((row, index) => (
-      <Td key={index} value={row} id={index}/>
-    ))
-    return <tr>{mapColumns}</tr>
+  const renderItem = (row, index) => <Td key={index} value={row} id={index}/>
+    // const mapColumns = row.map((row, index) => (
+    //   <Td key={index} value={row} id={index}/>
+    // ))
+    return <tr>{row.map(renderItem)}</tr>
 }
 
 export default tr
